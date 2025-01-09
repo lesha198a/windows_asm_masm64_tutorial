1fc36b75b422f364229f5b1d28d95e7a1f332e00
Oleksii Kalnytskyi <oleksii.kalnytskyi@gmail.com>
2025-01-09 23:55:06 +0200

    Add project files.

---
+++ asm/Source.asm
@@ -0,0 +1,28 @@
+includelib ucrt.lib
+includelib legacy_stdio_definitions.lib
+includelib msvcrt.lib
+
+option casemap:none
+
+.data
+fmtStr byte 'Hello', 10, 0
+
+.code
+externdef printf:proc
+externdef _CRT_INIT:proc
+externdef exit:proc
+
+main proc
+    call _CRT_INIT
+    push rbp
+    mov rbp, rsp
+
+    sub rsp, 32
+    lea rcx, fmtStr
+    call printf
+
+    xor ecx, ecx
+    call exit
+main endp
+
+end
